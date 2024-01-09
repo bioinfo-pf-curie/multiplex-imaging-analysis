f21d8c6e53b46ead038d26cdfb7f7b3fa7ee7bfd
mcorbe <maxime.corbe@curie.fr>
2024-01-09 17:17:30 +0100

    first try

--- bin/merge_segmentation.py
+++ bin/merge_segmentation.py
@@ -13,8 +13,11 @@ from cv2 import INTER_NEAREST
 from utils import OmeTifffile
 import sys
 
+import plotly.express as px
+import plotly.graph_objects as go
+import numcodecs
 
-def compute_masks(dP, cellprob, p=None, niter=200, 
+def compute_masks(flows, p=None, niter=200, 
                    cellprob_threshold=0.0,
                    flow_threshold=0.4, interp=True, 
                    min_size=15, resize=None, 
@@ -59,27 +62,33 @@ def compute_masks(dP, cellprob, p=None, niter=200,
     device: str
         name of the device where the calculation happen
     """
-    print(f"dP : {sys.getsizeof(dP)}, cellprob : {sys.getsizeof(cellprob)}")
+    dP = flows[:-1]
+    cellprob = flows[-1]
+    # print(f"dP : {sys.getsizeof(dP)}, cellprob : {sys.getsizeof(cellprob)}")
     
     cp_mask = cellprob > cellprob_threshold 
 
     if np.any(cp_mask): #mask at this point is a cell cluster binary map, not labels     
         # follow flows
         if p is None:
-            p, inds = follow_flows(dP * cp_mask / 5., niter=niter, interp=interp, 
-                                            use_gpu=use_gpu, device=device)
+            dP_da = da.from_array(dP * cp_mask / 5., chunks=[2, 256, 256])
+            p = da.map_overlap(follow_flows, dP_da, depth={0: 0, 1: 60, 2: 60}, 
+                                     niter=niter, interp=interp, use_gpu=use_gpu, device=device).to_zarr(".tmp_p.zarr", object_codec=numcodecs.JSON(), compute=True, return_stored=True)
+            # p, inds = follow_flows(dP * cp_mask / 5., niter=niter, interp=interp, 
+            #                                 use_gpu=use_gpu, device=device)
             print(f"p : {sys.getsizeof(p)}, {type(p)}")
-            if inds is None:
-                shape = resize if resize is not None else cellprob.shape
-                mask = np.zeros(shape, np.uint16)
-                p = np.zeros((len(shape), *shape), np.uint16)
-                return mask, p
+            # if inds is None:
+            #     shape = resize if resize is not None else cellprob.shape
+            #     mask = np.zeros(shape, np.uint16)
+            #     p = np.zeros((len(shape), *shape), np.uint16)
+            #     return mask, p
         #calculate masks
+
         mask = get_masks(p, iscell=cp_mask)
-        print(f"mask : {sys.getsizeof(mask)}, {type(mask)}")
+        p = None
+        # print(f"mask : {sys.getsizeof(mask)}, {type(mask)}")
             
         # flow thresholding factored out of get_masks
-        shape0 = p.shape[1:]
         if mask.max()>0 and flow_threshold is not None and flow_threshold > 0:
             # make sure labels are unique at output of get_masks
             mask = remove_bad_flow_masks(mask, dP, threshold=flow_threshold, use_gpu=use_gpu, device=device)
@@ -96,7 +105,7 @@ def compute_masks(dP, cellprob, p=None, niter=200,
             mask = resize_image(mask, resize[0], resize[1], interpolation=INTER_NEAREST)
             if recast:
                 mask = mask.astype(np.uint32)
-            Ly,Lx = mask.shape
+
         elif mask.max() < 2**16:
             mask = mask.astype(np.uint16)
 
@@ -104,15 +113,15 @@ def compute_masks(dP, cellprob, p=None, niter=200,
         shape = resize if resize is not None else cellprob.shape
         mask = np.zeros(shape, np.uint16)
         p = np.zeros((len(shape), *shape), np.uint16)
-        return mask, p
+        return mask
 
 
     # moving the cleanup to the end helps avoid some bugs arising from scaling...
     # maybe better would be to rescale the min_size and hole_size parameters to do the
     # cleanup at the prediction scale, or switch depending on which one is bigger... 
     mask = fill_holes_and_remove_small_masks(mask, min_size=min_size)
-
-    return mask, p
+    # print(f"{mask=}")
+    return mask[None, ...]
 
 
 def get_weight(tile, edge=False):
@@ -240,6 +249,62 @@ def stich_flow(list_npy, input_img_path, overlap):
         total_flow.flush()
     return total_flow
 
+# @profile
+def empty():
+    print('its just to see')
+
+
+color = px.colors.qualitative.Plotly
+color[0] = "black"
+color[7] = "white"
+
+def binarize(img):
+    res = (img - np.min(img)) / (np.max(img) - np.min(img))
+    res[res < .5] = 0
+    res[res >= .5] = 1
+    return res
+
+
+def compare(outline1, outline2):
+    return 100 * np.sum(binarize(outline1) - binarize(outline2)) / np.prod(outline1.shape) 
+
+def bool2int(x):
+    y = 0
+    for i,j in enumerate(x):
+        if j: y += int(j)<<i
+    return y
+
+
+def masks_comparison(list_of_masks, names=[]):
+    result = np.apply_along_axis(bool2int, 0, np.stack(list_of_masks).astype(bool))
+    if not names:
+        names = range(len(list_of_masks))
+    if len(list_of_masks) == 3:
+        names = {0: 'pas de masque', 1: names[0], 2: names[1], 
+                3: f'{names[0]} & {names[1]}', 4: names[2], 
+                5: f'{names[0]} & {names[2]}', 6: f'{names[1]} & {names[2]}',
+                7: "commun"}
+        colorscale = [[0, 'black'], [0.05, 'black'], 
+                      [0.05, '#EF553B'], [0.22, '#EF553B'], 
+                      [0.22, '#00CC96'], [0.35, '#00CC96'], 
+                      [0.35, '#AB63FA'], [0.5, '#AB63FA'], 
+                      [0.5, '#FFA15A'], [0.63, '#FFA15A'], 
+                      [0.63, '#19D3F3'], [0.77, '#19D3F3'], 
+                      [0.77, '#FF6692'], [0.95, '#FF6692'], 
+                      [0.95, 'white'], [1, 'white']]
+    elif len(list_of_masks) == 2:
+        names = {0: 'pas de masque', 1: names[0], 2: names[1], 3: "commun"}
+        colorscale = [[0, 'black'], [0.1, 'black'], 
+                      [0.1, '#EF553B'], [0.6, '#EF553B'], 
+                      [0.6, '#00CC96'], [0.9, '#00CC96'], 
+                      [0.9, 'white'], [1, 'white']]
+    else:
+        colorscale="sunset"
+    fig = go.Figure(go.Heatmap(z=result[::-1, :], colorscale=colorscale, colorbar=dict(tickvals=list(names.keys()), ticktext=list(names.values()))))
+    fig.update_yaxes(scaleanchor="x", scaleratio=1)
+    fig.show('browser')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--in', type=str, required=True, nargs='+', help="list of Image Path (cropped) to merge")
@@ -254,7 +319,17 @@ if __name__ == '__main__':
     else:
         flows = stich_flow(list_npy, args.original, overlap=args.overlap)
 
-    masks = compute_masks(flows[:-1], flows[-1])[0] # todo : modifier ça pour ne pas tout mettre en mémoire
+    import dask.array as da
+    from dask_image import ndmeasure
+    print(type(flows))
+
+    masks = compute_masks(flows)[0] # todo : modifier ça pour ne pas tout mettre en mémoire
+    # replacer 60 par 2 * la taille d'une cellule (pour etre sur de tout avoir)
+
+    # flows_da = da.from_array(flows, chunks=[3, 256, 256])
+    # masks_da = da.map_overlap(compute_masks, flows_da, dtype=np.uint16, depth={0: 0, 1: 60, 2: 60}).compute()[0].astype(bool)
+    # masks_da, _ = ndmeasure.label(masks_da)
+    # masks_comparison([masks, masks_da], names=['normal', 'dask'])
 
     metadata = OmeTifffile(TiffFile(args.original).pages[0])
     metadata.remove_all_channels()
@@ -262,3 +337,5 @@ if __name__ == '__main__':
     metadata.dtype = masks.dtype
 
     imwrite(args.out, masks, bigtiff=True, shaped=False, **metadata.to_dict())
+    # metadata.dtype = masks_da.dtype
+    # imwrite(args.out, masks_da, bigtiff=True, shaped=False, **metadata.to_dict())
