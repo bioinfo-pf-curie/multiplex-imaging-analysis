d909966a36896ec41edc31ea584a76a9e3181140
corbe_home2 <corbe@biologie.ens.fr>
2024-03-27 09:47:20 +0100

    ..

--- bin/single_cell_data_extraction.py
+++ bin/single_cell_data_extraction.py
@@ -136,8 +136,8 @@ def PrepareData(image,z, normalization=None, norm_val=None):
     
     if normalization is not None:
         print(f'{normalization == "auto"}')
-        print(f"{normalization == "auto" and norm_val is not None}")
-        print(f"{(normalization == "hist") or (normalization == "auto" and norm_val is not None)}")
+        print(f'{normalization == "auto" and norm_val is not None}')
+        print(f'{(normalization == "hist") or (normalization == "auto" and norm_val is not None)}')
         if (normalization == 'hist') or (normalization == "auto" and norm_val is not None): 
             print(f"compute hist (for {z})")
             nv = compute_hist(image_loaded_z[None, ...], 0, *image_loaded_z.shape, 256, 256, image_loaded_z.min(), image_loaded_z.max())
