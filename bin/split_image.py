955278da64ad0c00433c4da0da3b68a8ec1e1411
mcorbe <maxime.corbe@curie.fr>
2024-09-03 10:05:54 +0200

    change rescaling to a new parameter of split_image

--- bin/split_image.py
+++ bin/split_image.py
@@ -7,7 +7,7 @@ import argparse
 from utils import read_tiff_orion
 
 
-def split_img(img_path, out_dir, height=224, overlap=0.1, memory=0):
+def split_img(img_path, out_dir, height=224, overlap=0.1, memory=0, scaling=1):
     """Will split an image into height x image_width crop (with some overlap) to get a better memory footprint """
     img_name, ext = os.path.splitext(os.path.basename(img_path))
     img_zarr, metadata = read_tiff_orion(img_path, zarr_mode='a', mode='r+b')
@@ -17,8 +17,8 @@ def split_img(img_path, out_dir, height=224, overlap=0.1, memory=0):
     ch, total_height, total_width = img_zarr.shape
 
     if memory or not height:
-        computed_max_height = int(int(memory) / (img_zarr.dtype.itemsize * 8 * total_width * (ch+2)))
-        #                     memory_per_cpu / (size_of_pixel_in_bytes * nb_bit_per_byte * width * channel + 2 to get some margin)
+        computed_max_height = int(int(memory) * scaling / (img_zarr.dtype.itemsize * 8 * total_width * (ch+2)))
+        #                     memory_per_cpu * re-scaling of the image / (size_of_pixel_in_bytes * nb_bit_per_byte * width * channel + 2 to get some margin)
         height = min(height, computed_max_height) if height else computed_max_height
 
     for i, cur_height in enumerate(range(0, total_height, int(height * (1 - overlap))), 1):
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument('--height', type=int, required=False, help="height of tiles")
     parser.add_argument('--overlap', type=float, required=False, default=0.1, help="percentage of overlap for tiles")
     parser.add_argument('--memory', type=float, required=False, default=0, help="memory size available for each crop")
+    parser.add_argument('--scaling', type=float, required=False, default=1, help="scaling of image before seg (with another diameter than 30, image is rescaled beforehand)")
     args = parser.parse_args()
 
-    split_img(img_path=args.file_in, out_dir=args.out, height=args.height, overlap=args.overlap, memory=args.memory)
+    split_img(img_path=args.file_in, out_dir=args.out, height=args.height, overlap=args.overlap, memory=args.memory, scaling=args.scaling)
--- nf-modules/common/process/splitImage.nf
+++ nf-modules/common/process/splitImage.nf
@@ -17,12 +17,13 @@ process splitImage {
   script:
     // if params.segmentation.tileHeight is set, it will be passed into args
     // availableMem need to be scaled down if diameter if lower than 30 because of rescaling tile...
-    def availableMem = params.segmentation.tileHeight ? 0 : (params.segmentation.memory instanceof MemoryUnit ? params.segmentation.memory : MemoryUnit.of(params.segmentation.memory)) * (params.segmentation.diameter / 30)
+    def availableMem = params.segmentation.tileHeight ? 0 : (params.segmentation.memory instanceof MemoryUnit ? params.segmentation.memory : MemoryUnit.of(params.segmentation.memory)) 
     if (availableMem instanceof MemoryUnit) {
       availableMem = availableMem.getBytes()
     }
     def args = task.ext.args ?: ''
+    def scaling = params.segmentation.diameter / 30 // default is 1
     """
-    split_image.py --file_in $image --memory $availableMem $args
+    split_image.py --file_in $image --memory $availableMem --scaling $scaling $args
     """
 }
