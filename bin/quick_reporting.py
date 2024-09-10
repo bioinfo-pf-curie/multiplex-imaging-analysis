63bcb20641de58d617d5210741d537f4b591df03
mcorbe <maxime.corbe@curie.fr>
2024-09-10 17:35:05 +0200

    updated quick reporting with report capabilities

--- bin/quick_reporting.py
+++ bin/quick_reporting.py
@@ -1,6 +1,10 @@
 import argparse
 import scimap as sm
+from pathlib import Path
 import anndata as ad
+
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Image
 
 def prepare_data(csv_path):
     return sm.pp.mcmicro_to_scimap(csv_path)
@@ -8,29 +12,64 @@ def prepare_data(csv_path):
 def unsupervised_clustering(adata, method='leiden'):
     return sm.tl.cluster(adata, method=method, use_raw=False, log=False)
 
-def spatial_colormap(adata, color_by=['leiden_phenotype']):
-    return sm.pl.spatial_scatterPlot (adata, colorBy=color_by, figsize=(3,3), s=0.7, fontsize=5, catCmap='Set1')
+def compute_stuff(adata, method):
+    adata = unsupervised_clustering(adata, method)
+    adata = sm.tl.spatial_interaction(adata, phenotype=method,
+                                  method='radius', 
+                                  radius=70, 
+                                  label='spatial_interaction_radius')
+    adata = sm.tl.spatial_aggregate(adata, phenotype=method, 
+                                    method='radius', radius=50, purity=80, label='spatial_aggregate_radius')
+    return adata
+
+def spatial_colormap(adata, color_by=['leiden_phenotype'], filepath=".", **kwargs):
+    filepath = Path(filepath)
+    return sm.pl.spatial_scatterPlot (adata, colorBy=color_by, s=3, 
+                                      fontsize=5, catCmap='Set1', saveDir=filepath.parent, 
+                                      fileName=filepath.name, **kwargs)
+
+def spatial_interaction(adata, spatial_interaction='spatial_interaction_radius', filepath="."):
+    filepath = Path(filepath)
+    return sm.pl.spatialInteractionNetwork(adata, spatial_interaction=spatial_interaction, figsize=(6,4), saveDir=filepath.parent, fileName=filepath.name)
 
 def save(adata, out_path):
     adata.write(out_path)
 
-def write_report(imgs, report_path):
-    with open(report_path, 'w') as out:
-        for title, img in imgs: # or something like that
-            out.write(title)
-            out.write(img)
+def write_report(report_path):
+
+    doc = SimpleDocTemplate(report_path, pagesize=letter)
+    parts = []
+    parts.append(Image("scimap/spatial_interaction.jpg", width=400, height=600))
+    parts.append(Image("scimap/test_colormap.jpg", width=400, height=600))
+    parts.append(Image("scimap/test_interaction.jpg", width=400, height=600))
+    parts.append(Image("scimap/voronoi.jpg", width=400, height=600))
+    doc.build(parts)
 
 def main(csv_path, report_name, method):
     adata = prepare_data(csv_path)
-    adata = unsupervised_clustering(adata, method=method)
+    adata = compute_stuff(adata, method=method)
+    spatial_colormap(adata, color_by=method, filepath="scimap/test_colormap.jpg")
+    spatial_interaction(adata, filepath="scimap/test_interaction.jpg")
+    sm.pl.spatial_interaction(adata, 
+                          spatial_interaction='spatial_interaction_radius',
+                          linewidths=0.75, linecolor='black', figsize=(5,4), saveDir="scimap", fileName="spatial_interaction.jpg")
 
+    sm.pl.voronoi(adata, color_by='spatial_aggregate_radius', 
+                 voronoi_edge_color = 'black',
+                 voronoi_line_width = 0.3, 
+                 voronoi_alpha = 0.8, 
+                 size_max=3000,
+                 overlay_points=None, 
+                 plot_legend=True, saveDir="scimap", fileName="voronoi.jpg",
+                 legend_size=6)
 
+    write_report(report_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_path', type=str, required=True, help="path for csv file of quantification")
-    parser.add_argument('--report_name', type=str, required=False, help="Output filepath")
+    parser.add_argument('--report_name', type=str, required=True, help="Output filepath")
     parser.add_argument('--cluster_method', type=str, required=False, default="phenograph", 
                         help="name of the cluster method (currently available : kmeans, phenograph or leiden)")
     args = parser.parse_args()
