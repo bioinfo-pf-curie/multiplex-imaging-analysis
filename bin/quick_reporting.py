c6850a3473e72eaa57f1ee4299a7efbe95c7aa17
mcorbe <maxime.corbe@curie.fr>
2024-12-02 10:22:52 +0100

    comment unused parts

--- bin/quick_reporting.py
+++ bin/quick_reporting.py
@@ -15,65 +15,65 @@ import json
 
 from utils import min_max_norm
 
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import SimpleDocTemplate, Image, Table, Paragraph, Spacer, PageBreak
+# from reportlab.lib.pagesizes import A4
+# from reportlab.lib.styles import getSampleStyleSheet
+# from reportlab.platypus import SimpleDocTemplate, Image, Table, Paragraph, Spacer, PageBreak
 
 
 pio.templates.default = "plotly_white"
 
 
-class ScimapGraph:
-    def __init__(self, csv_path, method):
-        self.df = self.prepare_data(csv_path)
-        self.compute_stuff(method)
+# class ScimapGraph:
+#     def __init__(self, csv_path, method):
+#         self.df = self.prepare_data(csv_path)
+#         self.compute_stuff(method)
 
-        self.spatial_colormap(self.df, color_by=method, filepath="scimap/test_colormap.jpg")
-        self.spatial_interaction(self.df, filepath="scimap/test_interaction.jpg")
+#         self.spatial_colormap(self.df, color_by=method, filepath="scimap/test_colormap.jpg")
+#         self.spatial_interaction(self.df, filepath="scimap/test_interaction.jpg")
 
-        sm.pl.spatial_interaction(self.df, 
-                          spatial_interaction='spatial_interaction_radius',
-                          linewidths=0.75, linecolor='black', figsize=(5,4), saveDir="scimap", fileName="spatial_interaction.jpg")
-        sm.pl.heatmap(self.df, groupBy=method, standardScale="column",
-                    saveDir="scimap", fileName="cluster_phenotype.jpg")
+#         sm.pl.spatial_interaction(self.df, 
+#                           spatial_interaction='spatial_interaction_radius',
+#                           linewidths=0.75, linecolor='black', figsize=(5,4), saveDir="scimap", fileName="spatial_interaction.jpg")
+#         sm.pl.heatmap(self.df, groupBy=method, standardScale="column",
+#                     saveDir="scimap", fileName="cluster_phenotype.jpg")
 
-        sm.pl.voronoi(self.df, color_by='spatial_aggregate_radius', 
-                    voronoi_edge_color = 'black',
-                    voronoi_line_width = 0.3, 
-                    voronoi_alpha = 0.8, 
-                    size_max=3000,
-                    overlay_points=None,
-                    saveDir="scimap", fileName="voronoi.jpg",
-                    legend_size=6)
+#         sm.pl.voronoi(self.df, color_by='spatial_aggregate_radius', 
+#                     voronoi_edge_color = 'black',
+#                     voronoi_line_width = 0.3, 
+#                     voronoi_alpha = 0.8, 
+#                     size_max=3000,
+#                     overlay_points=None,
+#                     saveDir="scimap", fileName="voronoi.jpg",
+#                     legend_size=6)
     
-    @staticmethod
-    def prepare_data(csv_path):
-        return sm.pp.mcmicro_to_scimap(csv_path)
+#     @staticmethod
+#     def prepare_data(csv_path):
+#         return sm.pp.mcmicro_to_scimap(csv_path)
     
-    def compute_stuff(self, method):
-        self.unsupervised_clustering(method)
-        self.df = sm.tl.spatial_interaction(self.df, phenotype=method,
-                                    method='radius', 
-                                    radius=70, 
-                                    label='spatial_interaction_radius')
-        self.df = sm.tl.spatial_aggregate(self.df, phenotype=method, 
-                                        method='radius', radius=50, purity=80, label='spatial_aggregate_radius')
+#     def compute_stuff(self, method):
+#         self.unsupervised_clustering(method)
+#         self.df = sm.tl.spatial_interaction(self.df, phenotype=method,
+#                                     method='radius', 
+#                                     radius=70, 
+#                                     label='spatial_interaction_radius')
+#         self.df = sm.tl.spatial_aggregate(self.df, phenotype=method, 
+#                                         method='radius', radius=50, purity=80, label='spatial_aggregate_radius')
 
     
-    @staticmethod
-    def spatial_interaction(adata, spatial_interaction='spatial_interaction_radius', filepath="."):
-        filepath = Path(filepath)
-        return sm.pl.spatialInteractionNetwork(adata, spatial_interaction=spatial_interaction, figsize=(6,4), saveDir=filepath.parent, fileName=filepath.name)
+#     @staticmethod
+#     def spatial_interaction(adata, spatial_interaction='spatial_interaction_radius', filepath="."):
+#         filepath = Path(filepath)
+#         return sm.pl.spatialInteractionNetwork(adata, spatial_interaction=spatial_interaction, figsize=(6,4), saveDir=filepath.parent, fileName=filepath.name)
     
-    def unsupervised_clustering(self, method='leiden'):
-        self.df = sm.tl.cluster(self.df, method=method, use_raw=False, log=False)
+#     def unsupervised_clustering(self, method='leiden'):
+#         self.df = sm.tl.cluster(self.df, method=method, use_raw=False, log=False)
 
-    @staticmethod
-    def spatial_colormap(adata, color_by=['leiden_phenotype'], filepath=".", **kwargs):
-        filepath = Path(filepath)
-        return sm.pl.spatial_scatterPlot (adata, colorBy=color_by, s=3, 
-                                        fontsize=5, catCmap='Set1', saveDir=filepath.parent, 
-                                        fileName=filepath.name, **kwargs)
+#     @staticmethod
+#     def spatial_colormap(adata, color_by=['leiden_phenotype'], filepath=".", **kwargs):
+#         filepath = Path(filepath)
+#         return sm.pl.spatial_scatterPlot (adata, colorBy=color_by, s=3, 
+#                                         fontsize=5, catCmap='Set1', saveDir=filepath.parent, 
+#                                         fileName=filepath.name, **kwargs)
 
 class GetBasicInfo:
     cn = {
@@ -161,40 +161,40 @@ class GetBasicInfo:
         return self.df[self.marker_cols].corr()
 
 
-class PDFReport:
+# class PDFReport:
 
-    def __init__(self, reportpath, pagesize=A4):
-        self.styles = getSampleStyleSheet()
-        self.path = reportpath
-        self.doc = SimpleDocTemplate(self.path, pagesize=pagesize)
-        self.doc_width, self.doc_height = pagesize
-        self.parts = []
+#     def __init__(self, reportpath, pagesize=A4):
+#         self.styles = getSampleStyleSheet()
+#         self.path = reportpath
+#         self.doc = SimpleDocTemplate(self.path, pagesize=pagesize)
+#         self.doc_width, self.doc_height = pagesize
+#         self.parts = []
 
-    def p(self, text, style='BodyText'):
-        self.parts.append(Paragraph(text, style=self.styles[style]))
+#     def p(self, text, style='BodyText'):
+#         self.parts.append(Paragraph(text, style=self.styles[style]))
 
-    def header(self, title):
-        self.p(title, 'h1')
+#     def header(self, title):
+#         self.p(title, 'h1')
 
-    def spacer(self, height=100, width=None):
-        if width is None:
-            width = self.doc_width
-        self.parts.append(Spacer(width=width, height=height))
+#     def spacer(self, height=100, width=None):
+#         if width is None:
+#             width = self.doc_width
+#         self.parts.append(Spacer(width=width, height=height))
 
-    def img(self, src, width=None, height=None):
-        img = Image(src, width=width, height=height)
-        img.vAlign = "MIDDLE"
-        self.parts.append(img)
+#     def img(self, src, width=None, height=None):
+#         img = Image(src, width=width, height=height)
+#         img.vAlign = "MIDDLE"
+#         self.parts.append(img)
 
-    def page_break(self):
-        self.parts.append(PageBreak())
+#     def page_break(self):
+#         self.parts.append(PageBreak())
 
-    def write_report(self):
-        # parts.append(Image("scimap/spatial_interaction.jpg", width=400, height=560))
-        # parts.append(Image("scimap/test_colormap.jpg", width=400, height=560))
-        # parts.append(Image("scimap/test_interaction.jpg", width=400, height=560))
-        # parts.append(Image("scimap/voronoi.jpg", width=400, height=560))
-        self.doc.build(self.parts)
+#     def write_report(self):
+#         # parts.append(Image("scimap/spatial_interaction.jpg", width=400, height=560))
+#         # parts.append(Image("scimap/test_colormap.jpg", width=400, height=560))
+#         # parts.append(Image("scimap/test_interaction.jpg", width=400, height=560))
+#         # parts.append(Image("scimap/voronoi.jpg", width=400, height=560))
+#         self.doc.build(self.parts)
 
 def tiff2rgb(img, out_path="thumbnail.png"):
     color_cycle = [[int(h.strip("#")[i:i+2], 16) / 255 for i in (0, 2, 4)] 
@@ -210,42 +210,42 @@ def tiff2rgb(img, out_path="thumbnail.png"):
     return out_path
 
 
-def main(csv_path, image_path, report_name, method):
+# def main(csv_path, image_path, report_name, method):
     
-    info = GetBasicInfo(image_path, csv_path)
+#     info = GetBasicInfo(image_path, csv_path)
 
-    mypdf = PDFReport(report_name)
+#     mypdf = PDFReport(report_name)
     
-    mypdf.header(Path(csv_path).stem)
-    mypdf.img(info.th_img)#, width=200, height=200)
-    mypdf.spacer()
-    mypdf.p('Info', 'h3')
-    info_p = f"""
-- {info.nb_cell} cell{'s' if info.nb_cell > 1 else ''} found<br />
-- Tissue / Background area : {info.tissue_fraction*100:.02f} %<br />
-- Segmented Fraction : {info.segmented_fraction*100:.02f} %<br />
-"""
-    if filter_data:
-        info_p += f"""
-- Filter : {filter_data}<br />
-"""
-    mypdf.p(info_p)
+#     mypdf.header(Path(csv_path).stem)
+#     mypdf.img(info.th_img)#, width=200, height=200)
+#     mypdf.spacer()
+#     mypdf.p('Info', 'h3')
+#     info_p = f"""
+# - {info.nb_cell} cell{'s' if info.nb_cell > 1 else ''} found<br />
+# - Tissue / Background area : {info.tissue_fraction*100:.02f} %<br />
+# - Segmented Fraction : {info.segmented_fraction*100:.02f} %<br />
+# """
+#     if filter_data:
+#         info_p += f"""
+# - Filter : {filter_data}<br />
+# """
+#     mypdf.p(info_p)
     
-    mypdf.page_break()
-    mypdf.p('Size Distribution', 'h3')
-    mypdf.img(info.size_dis, width=mypdf.doc.width, height=400)
-    mypdf.page_break()
-    mypdf.p('Markers Distribution', 'h3')
-    mypdf.img(info.marker_dis, width=mypdf.doc.width, height=400)
-    mypdf.page_break()
-    mypdf.p('Markers Co-Distribution', 'h3')
-    mypdf.img(info.coexpr, width=mypdf.doc.width, height=600)
-    mypdf.p('Materials and Methods', 'h3')
-    mypdf.p("""
+#     mypdf.page_break()
+#     mypdf.p('Size Distribution', 'h3')
+#     mypdf.img(info.size_dis, width=mypdf.doc.width, height=400)
+#     mypdf.page_break()
+#     mypdf.p('Markers Distribution', 'h3')
+#     mypdf.img(info.marker_dis, width=mypdf.doc.width, height=400)
+#     mypdf.page_break()
+#     mypdf.p('Markers Co-Distribution', 'h3')
+#     mypdf.img(info.coexpr, width=mypdf.doc.width, height=600)
+#     mypdf.p('Materials and Methods', 'h3')
+#     mypdf.p("""
 
-""")
+# """)
 
-    mypdf.write_report()
+#     mypdf.write_report()
 
 def main2(image_path, csv_path, qcparms, out_dir):
     df_gen = {}
