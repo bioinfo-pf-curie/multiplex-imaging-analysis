0d1c4e51d41195cf9483a9550d26e0710b4b7c8f
mcorbe <maxime.corbe@curie.fr>
2025-01-20 17:44:54 +0100

    still WIP

--- bin/Hyperion_converter.py
+++ bin/Hyperion_converter.py
@@ -44,13 +44,42 @@ def read_acquisition(f, acquisition):
         )
     num_pixels = data_size // bytes_per_pixel
     f._fh.seek(0)
-    return np.memmap(
+    data = np.memmap(
         f._fh,
         dtype=np.float32,
         mode="r",
         offset=data_start_offset,
         shape=(num_pixels, num_channels + 3),
     )
+    xs = data[:, 0].astype(int)
+    ys = data[:, 1].astype(int)
+    try:
+        width = int(acquisition.metadata["MaxX"])
+        height = int(acquisition.metadata["MaxY"])
+        if width <= np.amax(xs) or height <= np.amax(ys):
+            raise ValueError(
+                "data shape is incompatible with acquisition image dimensions"
+            )
+    except (KeyError, ValueError):
+        warn(
+            f"MCD file '{f.path.name}' corrupted: "
+            "cannot read acquisition image dimensions; recovering from data shape"
+        )
+        width = np.amax(xs) + 1
+        height = np.amax(ys) + 1
+    if width * height != data.shape[0]:
+        if strict:
+            raise IOError(
+                f"MCD file '{f.path.name}' corrupted: "
+                "inconsistent acquisition image data size"
+            )
+        warn(
+            f"MCD file '{f.path.name}' corrupted: "
+            "inconsistent acquisition image data size"
+        )
+    img = np.memmap('.tmp.npy', shape=(num_channels, height, width), mode="w+", dtype=np.float32)
+    img[:, ys, xs] = np.transpose(data[:, 3:])
+    return img
 
 def mcd2ometiff(image_path):
     with MCDFile(image_path) as f:
