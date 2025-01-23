088caacc16ff89c7dda63f2259d3e57e70246c7b
mcorbe <maxime.corbe@curie.fr>
2025-01-23 14:40:08 +0100

    try with transform

--- bin/stitch_masks.py
+++ bin/stitch_masks.py
@@ -22,35 +22,35 @@ if __name__ == '__main__':
     original_tiff = tifffile.TiffFile(args.original)
     original_shape = original_tiff.series[0].shape[1:]
     out_path = f"{Path(args.original).stem}_masks.tiff"
-    tmp_path = ".tmp_masks.npy"
-    result = np.lib.format.open_memmap(tmp_path, mode='w+', dtype=np.uint32, shape=original_shape)
+    # tmp_path = ".tmp_masks.npy"
+    # result = np.lib.format.open_memmap(tmp_path, mode='w+', dtype=np.uint32, shape=original_shape)
     # result = tifffile.memmap(out_path, dtype="uint32", shape=original_shape, mode='w+')
-    px_overlap = int(original_shape[0] * args.overlap)
-    
-    for i, tile in enumerate(list_npy):
-        print(f"in tile : '{tile}'")
-        cur_height = get_current_height(tile)
-        img = tifffile.imread(tile)
+    # px_overlap = int(original_shape[0] * args.overlap)
+    result = merge_masks(list_npy, chunk_size=8192, transform=[(0, get_current_height(tile)) for tile in list_npy])
+    # for i, tile in enumerate(list_npy):
+    #     print(f"in tile : '{tile}'")
+    #     cur_height = get_current_height(tile)
+    #     img = tifffile.imread(tile)
 
-        starting_point = max(cur_height - px_overlap, 0)
-        ending_point = min(cur_height + img.shape[0] + px_overlap, original_shape[0])
+    #     starting_point = max(cur_height - px_overlap, 0)
+    #     ending_point = min(cur_height + img.shape[0] + px_overlap, original_shape[0])
 
-        print((starting_point, ending_point))
-        print(f"value before : {result[starting_point: ending_point, :].shape}")
-        augmented_img = np.pad(img.astype('uint32'), ((starting_point and px_overlap, (ending_point != original_shape[0]) and px_overlap), (0,0)))
-        print(f"img before : {augmented_img.shape}")
-        r = merge_masks([result[starting_point:ending_point, :], augmented_img], chunk_size=8192, remap=False) # , transform=[(0,0), (0, starting_point and px_overlap)]
+    #     print((starting_point, ending_point))
+    #     print(f"value before : {result[starting_point: ending_point, :].shape}")
+    #     augmented_img = np.pad(img.astype('uint32'), ((starting_point and px_overlap, (ending_point != original_shape[0]) and px_overlap), (0,0)))
+    #     print(f"img before : {augmented_img.shape}")
+    #     r = merge_masks([result[starting_point:ending_point, :], augmented_img], chunk_size=8192, remap=False) # , transform=[(0,0), (0, starting_point and px_overlap)]
 
-        print(r.max())
-        result[starting_point:ending_point, :] = r
+    #     print(r.max())
+    #     result[starting_point:ending_point, :] = r
 
-        if not i % 10: # flush every ten file (~10GB)
-            result.flush()
-            # reload memmap each time else it will accumulate in memory
-            result = np.lib.format.open_memmap(tmp_path, mode="r+")
-    result.flush()
+    #     if not i % 10: # flush every ten file (~10GB)
+    #         result.flush()
+    #         # reload memmap each time else it will accumulate in memory
+    #         result = np.lib.format.open_memmap(tmp_path, mode="r+")
+    # result.flush()
 
-    fastremap.renumber(result, in_place=True)
+    # fastremap.renumber(result, in_place=True)
 
     metadata = OmeTifffile(original_tiff.pages[0])
     metadata.remove_all_channels()
@@ -62,4 +62,4 @@ if __name__ == '__main__':
     kwargs = metadata.to_dict()
 
     tifffile.imwrite(out_path, result, bigtiff=True, shaped=False, **kwargs)
-    Path(tmp_path).unlink()
+    # Path(tmp_path).unlink()
