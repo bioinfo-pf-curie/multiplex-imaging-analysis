eac8be0c6917c5b3e0e6823f94593b54f2bd4674
mcorbe <maxime.corbe@curie.fr>
2025-01-23 10:17:56 +0100

    use numpy memmap

--- bin/stitch_masks.py
+++ bin/stitch_masks.py
@@ -3,9 +3,11 @@
 import argparse
 import tifffile
 from pathlib import Path
+import numpy as np
+import fastremap
 
 from merge_masks import merge_masks
-from utils import get_current_height
+from utils import get_current_height, OmeTifffile
 
 
 if __name__ == '__main__':
@@ -20,14 +22,17 @@ if __name__ == '__main__':
     original_tiff = tifffile.TiffFile(args.original)
     original_shape = original_tiff.series[0].shape[1:]
     out_path = f"{Path(args.original).stem}_masks.tiff"
-    result = tifffile.memmap(out_path, dtype="uint32", shape=original_shape, mode='w+')
+    tmp_path = ".tmp_masks.npy"
+    result = np.lib.format.open_memmap(tmp_path, mode='w+', dtype=np.uint32, shape=original_shape)
+    # result = tifffile.memmap(out_path, dtype="uint32", shape=original_shape, mode='w+')
     
     for i, tile in enumerate(list_npy):
         print(f"in tile : '{tile}'")
         cur_height = get_current_height(tile)
         img = tifffile.imread(tile)
         print((cur_height, cur_height + img.shape[0]))
-        print(img.dtype)
+        print(f"value before : {result[cur_height:cur_height + img.shape[0], :].max()}")
+        print(f"img before : {img.astype('uint32').max()}")
         r = merge_masks([result[cur_height:cur_height + img.shape[0], :], img.astype('uint32')], chunk_size=8192)
         print(r.max())
         result[cur_height:cur_height + img.shape[0], :] = r
@@ -35,6 +40,19 @@ if __name__ == '__main__':
         if not i % 10: # flush every ten file (~10GB)
             result.flush()
             # reload memmap each time else it will accumulate in memory
-            result = tifffile.memmap(out_path, dtype="uint32", shape=original_shape, mode="r+")
+            result = np.lib.format.open_memmap(tmp_path, mode="r+")
     result.flush()
-    # result[...] = merge_masks(list_npy, overlap=args.overlap, chunk_size=8192)
+
+    fastremap.renumber(result, in_place=True)
+
+    metadata = OmeTifffile(original_tiff.pages[0])
+    metadata.remove_all_channels()
+    metadata.add_channel_metadata(channel_name="masks")
+
+    metadata.dtype = result.dtype
+    metadata.update_shape(result.shape)
+
+    kwargs = metadata.to_dict()
+
+    tifffile.imwrite(args.out, result, bigtiff=True, shaped=False, **kwargs)
+    Path(tmp_path).unlink()
