88d34e1e148954b5dfab6e9e72b1c8c2087ffab1
mcorbe <maxime.corbe@curie.fr>
2025-01-23 17:03:17 +0100

    reduce chunk size

--- bin/stitch_masks.py
+++ bin/stitch_masks.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     # result = tifffile.memmap(out_path, dtype="uint32", shape=original_shape, mode='w+')
     # px_overlap = int(original_shape[0] * args.overlap)
     resulting_shape = np.zeros(original_shape)
-    result = merge_masks([resulting_shape] + list_npy, chunk_size=4096, transform=[(0,0)] + [(0, get_current_height(tile)) for tile in list_npy])
+    result = merge_masks([resulting_shape] + list_npy, transform=[(0,0)] + [(0, get_current_height(tile)) for tile in list_npy])
     # for i, tile in enumerate(list_npy):
     #     print(f"in tile : '{tile}'")
     #     cur_height = get_current_height(tile)
